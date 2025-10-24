b5932bbadb103a096fbe844323e7277fd67d1b80
tattiii8 <t46.cloud@gmail.com>
2025-10-25 02:36:41 +0900

    terraform

---
+++ docker_terraform.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+export AWS_ACCESS_KEY_ID="AWS_ACCESS_KEY_ID" 
+export AWS_SECRET_ACCESS_KEY="AWS_SECRET_ACCESS_KEY" 
+export AWS_SESSION_TOKEN="AWS_SESSION_TOKEN" 
+export TERRAFORM_VERSION="1.6.0" 
+
+TERRAFORM_VERSION="${TERRAFORM_VERSION}" 
+
+docker run --rm \
+  -v //Users/tatsuki/terraform:/terraform \
+  -w /terraform \
+  -e AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY \
+  -e AWS_SESSION_TOKEN \
+  -e AWS_DEFAULT_REGION="ap-northeast-1" \
+  -it --entrypoint=ash \
+  hashicorp/terraform:${TERRAFORM_VERSION} \
